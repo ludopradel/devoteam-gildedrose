01a4dc7442fd21f80492801e64f93431cc7eebf0
Mahdi0991 <mgarrach@devoteam.com>
2020-05-29 16:59:14 +0200

    Add duplication

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,17 +14,7 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
+                if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality < 50)
                     {
@@ -50,36 +40,53 @@ namespace csharp
                         }
                     }
                 }
+                else
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                        {
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                }
+                else
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
+                    if (Items[i].Name == "Aged Brie")
                     {
                         if (Items[i].Quality < 50)
                         {
                             Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                        else
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                                {
+                                    continue;
+                                }
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
                         }
                     }
                 }
